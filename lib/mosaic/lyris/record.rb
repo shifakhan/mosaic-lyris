6daf7cb7725ab8a310087c1b3eaec7e6a5aba4de
brent <brent@ecfcc2fa-3efd-41c1-a4d5-d3fffe500f87>
2009-11-17 16:35:23 +0000

    working record queries

--- lib/mosaic/lyris/record.rb
+++ lib/mosaic/lyris/record.rb
@@ -1,17 +1,49 @@
 module Mosaic
   module Lyris
     class Record < Object
-      attr_reader :id,
-                  :list_id
+      attr_reader :demographics,
+                  :doubleoptin,
+                  :email,
+                  :encoding,
+                  :id,
+                  :list_id,
+                  :proof,
+                  :state,
+                  :statedate,
+                  :trashed,
+                  :trigger
 
       class << self
-        def query(what, list_id)
+        def add(list_id, email, options = {})
+          validate_options!(options)
+          reply = post('record', 'add') do |request|
+            request.MLID list_id
+            put_data(request, 'email', email)
+            put_demographic_data(request, options[:demographics])
+            put_extra_data(request, 'trigger', 'yes') if options[:trigger]
+            put_extra_data(request, 'proof', 'yes') if options[:proof]
+            put_extra_data(request, 'state', options[:state])
+            put_extra_data(request, 'state', options[:encoding])
+            put_extra_data(request, 'state', 'yes') if options[:doubleoptin]
+          end
+          new(options.merge(:id => reply.at('/DATASET/DATA').html.to_i, :list_id => list_id, :name => name, :type => type))
+        end
+
+        def query(what, list_id, options = {})
           reply = post('record', query_type(what)) do |request|
             request.MLID list_id
+            put_extra_data(request, 'pagelimit', options[:per_page])
+            put_extra_data(request, 'page', options[:page] || 1) if options[:per_page]
           end
           reply.search('/DATASET/RECORD').collect do |record|
-            new :id => get_integer_data(record, 'id'),
-                :list_id => list_id
+            new :demographics => get_demographic_data(record),
+                :email => get_data(record, 'email'),
+                :id => get_data(record, 'extra', nil, :id => 'uid'),
+                :list_id => list_id,
+                :proof => get_boolean_data(record, 'extra', 'yes', nil, :id => 'proof'),
+                :state => get_data(record, 'extra', nil, :id => 'state') || 'active',
+                :statedate => get_date_data(record, 'extra', nil, :id => 'statedate'),
+                :trashed => get_boolean_data(record, 'extra', 'y', nil, :id => 'trashed')
           end
         end
 
@@ -19,6 +51,10 @@ module Mosaic
         def query_type(what)
           raise ArgumentError, "expected :all; got #{what.inspect}" unless %w(all).include?(what.to_s)
           'query-listdata'
+        end
+
+        def validate_options!(options)
+          raise ArgumentError, "expected state of :active, :bounced, :unsubscribed or :trashed; got #{options[:state]}" unless %w(active bounced unsubscribed trashed).include?(options[:state].to_s) if options[:state]
         end
       end
     end
--- test/record_test.rb
+++ test/record_test.rb
@@ -1,18 +1,79 @@
 require File.join(File.dirname(__FILE__),'test_helper')
 
 class TestRecord < Test::Unit::TestCase
-  def teardown
-    Net::HTTP.block_requests
-  end
-
   def test_bad_query
     assert_raises ArgumentError do
-      demographics = Mosaic::Lyris::Record.query(:bad, 1)
+      records = Mosaic::Lyris::Record.query(:bad, 1)
     end
   end
 
+  RECORDS = [
+    { :email => 'one@one.not', :proof => true, :trashed => false, :state => 'active' },
+    { :email => 'two@two.not', :proof => true, :trashed => false, :state => 'active' },
+    { :email => 'three@three.not', :proof => true, :trashed => false, :state => 'active' },
+    { :email => 'four@four.not', :proof => true, :trashed => false, :state => 'active' },
+    { :email => 'five@five.not', :trashed => false, :state => 'active' },
+    { :email => 'six@six.not', :trashed => true, :state => 'admin', :statedate => Date.new(2006,6,1) },
+    { :email => 'seven@seven.not', :trashed => true, :state => 'unsubscribed', :statedate => Date.new(2007,7,1) },
+    { :email => 'eight@eight.not', :trashed => true, :state => 'bounced', :statedate => Date.new(2008,8,1) },
+    { :email => 'nine@nine.not', :trashed => false, :state => 'active' },
+    { :email => 'ten@ten.not', :trashed => false, :state => 'active' }
+  ]
+
+  DEMOGRAPHICS = [
+    { 1 => 'on', 2 => '01/02/03', 3 => %w(one three), 4 => %w(two four), 5 => 'three', 6 => 'six', 7 => 'value for every demographic', 8 => 'the date value in demographic 2 should be Jan 2, 2003' },
+    { 1 => 'on', 2 => '02/22/22', 5 => 'three', 6 => 'six', 7 => 'no value for multiple selection demographics', 8 => 'the date value in demographic 2 should be Feb 22, 1922' },
+    { 1 => 'on', 2 => '03/03/33', 3 => %w(one three), 4 => %w(two four), 5 => 'three', 6 => 'six', 7 => 'every attribute enabled', 8 => 'the date value in demographic 2 should be Mar 3, 1933' },
+    nil,
+    { 7 => 'active' },
+    { 7 => 'trashed by administrator' },
+    { 7 => 'unsubscribed' },
+    { 7 => 'bounced' },
+  ]
+
   def test_query_all
-    Net::HTTP.block_requests false
-    demographics = Mosaic::Lyris::Record.query(:all, 1)
+    records = Mosaic::Lyris::Record.query(:all, 1)
+    assert_instance_of Array, records
+    assert_equal 10, records.size
+    records.each_with_index do |r,i|
+      assert_instance_of Mosaic::Lyris::Record, r
+      assert_equal 'abcdef%04d' % (i+1), r.id
+      assert_equal RECORDS[i][:email], r.email
+      assert_equal RECORDS[i][:proof], r.proof
+      assert_equal RECORDS[i][:trashed], r.trashed
+      assert_equal RECORDS[i][:state], r.state
+      assert_equal RECORDS[i][:statedate], r.statedate
+      assert_equal DEMOGRAPHICS[i], r.demographics
+    end
+  end
+
+  def test_query_all_empty
+    records = Mosaic::Lyris::Record.query(:all, 2)
+    assert_instance_of Array, records
+    assert_equal 0, records.size
+  end
+
+  def test_query_all_paginated
+    i = 0
+    (1..3).each do |page|
+      records = Mosaic::Lyris::Record.query(:all, 3, :page => page, :per_page => 4)
+      assert_instance_of Array, records
+      if page < 3
+        assert_equal 4, records.size
+      else
+        assert_equal 2, records.size
+      end
+      records.each do |r|
+        assert_instance_of Mosaic::Lyris::Record, r
+        assert_equal 'abcdef%04d' % (i+1), r.id
+        assert_equal RECORDS[i][:email], r.email
+        assert_equal RECORDS[i][:proof], r.proof
+        assert_equal RECORDS[i][:trashed], r.trashed
+        assert_equal RECORDS[i][:state], r.state
+        assert_equal RECORDS[i][:statedate], r.statedate
+        assert_equal DEMOGRAPHICS[i], r.demographics
+        i += 1
+      end
+    end
   end
 end
