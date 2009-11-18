ff9e2d7da3e4cd84104233ea64260f152a5159dc
brent <brent@ecfcc2fa-3efd-41c1-a4d5-d3fffe500f87>
2009-11-18 13:52:40 +0000

    test stubs

---
+++ test/partner_test.rb
@@ -0,0 +1,4 @@
+require File.join(File.dirname(__FILE__),'test_helper')
+
+class TestPartner < Test::Unit::TestCase
+end
---
+++ test/trigger_test.rb
@@ -0,0 +1,4 @@
+require File.join(File.dirname(__FILE__),'test_helper')
+
+class TestTrigger < Test::Unit::TestCase
+end
