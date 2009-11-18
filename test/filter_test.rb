d4086c85c771dc98687cb0a02038f2b11be4a1fb
brent <brent@ecfcc2fa-3efd-41c1-a4d5-d3fffe500f87>
2009-11-18 16:11:08 +0000

    test stubs

--- test/filter_test.rb
+++ test/filter_test.rb
@@ -1,4 +1,6 @@
 require File.join(File.dirname(__FILE__),'test_helper')
 
 class TestFilter < Test::Unit::TestCase
+  def test_stub
+  end
 end
--- test/message_test.rb
+++ test/message_test.rb
@@ -1,4 +1,6 @@
 require File.join(File.dirname(__FILE__),'test_helper')
 
 class TestMessage < Test::Unit::TestCase
+  def test_stub
+  end
 end
