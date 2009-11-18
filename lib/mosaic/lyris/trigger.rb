43573b5a5efcd556b3e78452fe189e0a7fcd86f1
brent <brent@ecfcc2fa-3efd-41c1-a4d5-d3fffe500f87>
2009-11-18 16:11:39 +0000

    basic trigger support

--- lib/mosaic/lyris/trigger.rb
+++ lib/mosaic/lyris/trigger.rb
@@ -1,11 +1,21 @@
 module Mosaic
   module Lyris
     class Trigger < Object
+      attr_reader :id,
+                  :not_sent,
+                  :sent
+
       class << self
-        def fire(id, recipients, options = {})
+        def fire(id, *recipients)
+          options = recipients.pop if recipients.last.is_a?(Hash)
           reply = post('triggers', 'fire-trigger') do |request|
             request.MLID options[:list_id] if options[:list_id]
+            put_data(request, 'extra', id, :id => 'trigger_id')
+            put_data(request, 'extra', recipients.join(','), :id => 'recipients')
           end
+          sent = get_data(reply.at('/DATASET'), 'sent') || ''
+          not_sent = get_data(reply.at('/DATASET'), 'not sent') || ''
+          new :id => id, :not_sent => not_sent.split(','), :sent => sent.split(',')
         end
       end
     end
--- test/trigger_test.rb
+++ test/trigger_test.rb
@@ -1,4 +1,26 @@
 require File.join(File.dirname(__FILE__),'test_helper')
 
 class TestTrigger < Test::Unit::TestCase
+  def teardown
+    Net::HTTP.block_requests
+  end
+
+  def test_fire
+    trigger = Mosaic::Lyris::Trigger.fire(1, 'one@email.not', 'two@email.not', 'three@email.not', :list_id => 1)
+    assert_instance_of Mosaic::Lyris::Trigger, trigger
+    assert_equal %w(one@email.not two@email.not three@email.not), trigger.sent
+    assert_equal %w(four@email.not), trigger.not_sent
+  end
+
+  def test_fire_invalid_recipients
+    assert_raise Mosaic::Lyris::Error do
+      Mosaic::Lyris::Trigger.fire(1, 'invalid@email.not', :list_id => 1)
+    end
+  end
+
+  def test_fire_invalid_trigger_id
+    assert_raise Mosaic::Lyris::Error do
+      Mosaic::Lyris::Trigger.fire(666, 'one@email.not', :list_id => 1)
+    end
+  end
 end
