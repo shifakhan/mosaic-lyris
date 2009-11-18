687e4b6736d7e2c27dea1331e03189e729dc90df
brent <brent@ecfcc2fa-3efd-41c1-a4d5-d3fffe500f87>
2009-11-18 13:50:37 +0000

    correct assignment of default options

--- lib/mosaic/lyris/trigger.rb
+++ lib/mosaic/lyris/trigger.rb
@@ -2,7 +2,7 @@ module Mosaic
   module Lyris
     class Trigger < Object
       class << self
-        def fire(id, recipients, options => {})
+        def fire(id, recipients, options = {})
           reply = post('triggers', 'fire-trigger') do |request|
             request.MLID options[:list_id] if options[:list_id]
           end
