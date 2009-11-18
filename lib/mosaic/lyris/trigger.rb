22860da8c4a0f5555c5d94d00795979450890ddc
brent <brent@ecfcc2fa-3efd-41c1-a4d5-d3fffe500f87>
2009-11-18 16:44:32 +0000

    support for trigger parameters

--- lib/mosaic/lyris/trigger.rb
+++ lib/mosaic/lyris/trigger.rb
@@ -1,21 +1,31 @@
 module Mosaic
   module Lyris
     class Trigger < Object
-      attr_reader :id,
+      attr_reader :add,
+                  :clickthru,
+                  :id,
+                  :message,
                   :not_sent,
-                  :sent
+                  :recipient_data,
+                  :sent,
+                  :subject
 
       class << self
         def fire(id, *recipients)
           options = recipients.pop if recipients.last.is_a?(Hash)
           reply = post('triggers', 'fire-trigger') do |request|
             request.MLID options[:list_id] if options[:list_id]
-            put_data(request, 'extra', id, :id => 'trigger_id')
-            put_data(request, 'extra', recipients.join(','), :id => 'recipients')
+            put_extra_data(request, 'trigger_id', id)
+            put_extra_data(request, 'recipients', recipients.join(','))
+            put_extra_data(request, 'recipient_data', options[:recipient_data])
+            put_extra_data(request, 'subject', options[:subject])
+            put_extra_data(request, 'clickthru', 'on') if options[:clickthru]
+            put_extra_data(request, 'add', 'yes') if options[:add]
+            put_extra_data(request, 'message', options[:message])
           end
           sent = get_data(reply.at('/DATASET'), 'sent') || ''
           not_sent = get_data(reply.at('/DATASET'), 'not sent') || ''
-          new :id => id, :not_sent => not_sent.split(','), :sent => sent.split(',')
+          new options.merge(:id => id, :not_sent => not_sent.split(','), :sent => sent.split(','))
         end
       end
     end
--- test/trigger_test.rb
+++ test/trigger_test.rb
@@ -5,11 +5,30 @@ class TestTrigger < Test::Unit::TestCase
     Net::HTTP.block_requests
   end
 
+  MESSAGE = <<END_OF_MESSAGE
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>this is a test</title>
+</head>
+<body style="margin:0px;padding:0px; background-color: #333; outline:0px;">
+<h1 style="color: #FFF;">THIS IS A TEST</h1>
+<p style="color: #CCC;">hello</p>
+<p style="color: #CCC;">test message to: <strong style="color: #FFF;">%%EMAIL_ADDRESS%%</strong></p>
+<p style="color: #CCC;">goodbye</p>
+</body>
+</html>
+END_OF_MESSAGE
+
   def test_fire
-    trigger = Mosaic::Lyris::Trigger.fire(1, 'one@email.not', 'two@email.not', 'three@email.not', :list_id => 1)
+    trigger = Mosaic::Lyris::Trigger.fire(1, 'one@email.not', 'two@email.not', 'three@email.not', :list_id => 1, :subject => 'this is a test', :clickthru => true, :add => true, :message => MESSAGE)
     assert_instance_of Mosaic::Lyris::Trigger, trigger
     assert_equal %w(one@email.not two@email.not three@email.not), trigger.sent
     assert_equal %w(four@email.not), trigger.not_sent
+    assert_equal 'this is a test', trigger.subject
+    assert_equal true, trigger.clickthru
+    assert_equal true, trigger.add
+    assert_equal MESSAGE, trigger.message
   end
 
   def test_fire_invalid_recipients
