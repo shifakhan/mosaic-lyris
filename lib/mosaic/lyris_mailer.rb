26be0848b2987f7a9e54b0dd8a0427f26d20e41c
Matthew Eagar <matthew.eagar@mosaic.ca>
2011-06-22 15:56:41 -0400

    Update for Rails3

--- lib/mosaic/lyris_mailer.rb
+++ lib/mosaic/lyris_mailer.rb
@@ -1,17 +1,24 @@
 module Mosaic
-  module LyrisMailer
+  class LyrisMailer
     include ActionView::Helpers::TagHelper
     include ActionView::Helpers::TextHelper
     include ActionView::Helpers::SanitizeHelper
+
+    def initialize(values = {})
+    end
+
+    def deliver!(mail)
+      perform_delivery_lyris(mail)
+    end
 
   private
     def get_lyris_html(mail)
       if mail.multipart?
         get_part_body(mail, 'text/html') || simple_format(get_part_body(mail, 'text/plain'))
-      elsif mail.content_type == 'text/html'
+      elsif mail.content_type.start_with? 'text/html'
         get_part_body(mail, 'text/html')
-      elsif mail.content_type == 'text/plain'
-        simple_format(mail.body)
+      elsif mail.content_type.start_with? 'text/plain'
+        simple_format(mail.body.to_s)
       else
         raise TypeError, "unable to retrieve text/html for content type (#{mail.content_type})"
       end
@@ -22,7 +29,7 @@ module Mosaic
       lyris_options = {}
       lyris_options[:subject] = mail.subject
       lyris_options[:from_email] = mail.from
-      lyris_options[:from_name] = mail.friendly_from
+      lyris_options[:from_name] = mail[:from].formatted#.friendly_from
       lyris_options[:clickthru] = true
       lyris_options[:message] = get_lyris_html(mail)
       lyris_options[:message_text] = get_lyris_text(mail)
@@ -32,9 +39,9 @@ module Mosaic
     def get_lyris_text(mail)
       if mail.multipart?
         get_part_body(mail, 'text/plain') || strip_tags(get_part_body(mail, 'text/html'))
-      elsif mail.content_type == 'text/plain'
-        mail.body
-      elsif mail.content_type == 'text/html'
+      elsif mail.content_type.start_with? 'text/plain'
+        mail.body.to_s
+      elsif mail.content_type.start_with? 'text/html'
         strip_tags(get_part_body(mail, 'text/html'))
       else
         raise TypeError, "unable to retrieve text/plain for content type (#{mail.content_type})"
@@ -42,12 +49,13 @@ module Mosaic
     end
 
     def get_part(mail, content_type)
-      mail.parts.find { |part| part.content_type == content_type }
+      return mail if mail.parts.length == 0
+      mail.parts.find { |part| part.content_type.start_with? content_type }
     end
 
     def get_part_body(mail, content_type)
       part = get_part(mail, content_type)
-      part.body if part
+      part.body.to_s if part
     end
 
     def perform_delivery_lyris(mail)
@@ -60,7 +68,10 @@ module Mosaic
       # raise "triggered email not sent" unless trigger.sent.include?(email)
     end
   end
+
 end
 
-ActionMailer::Base.send :include, Mosaic::LyrisMailer
-ActionMailer::Base.send :extend, ActionView::Helpers::SanitizeHelper::ClassMethods
+ActionMailer::Base::add_delivery_method :lyris, Mosaic::LyrisMailer
+
+#ActionMailer::Base.send :include, Mosaic::LyrisMailer
+#ActionMailer::Base.send :extend, ActionView::Helpers::SanitizeHelper::ClassMethods
